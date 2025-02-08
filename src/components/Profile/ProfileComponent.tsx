b93369b92f48f6eb5e9138877668f3b6b5764d1b
ductaip <dtai858@gmail.com>
2025-02-08 23:27:28 +0700

    config update profile api with react query

--- src/components/Profile/ProfileComponent.tsx
+++ src/components/Profile/ProfileComponent.tsx
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query"
+import { useMutation, useQuery } from "@tanstack/react-query"
 import userApi from "@uth/apis/user.api"
 import Input from "@uth/components/Input"
 import Button from "@uth/components/Button"
@@ -6,6 +6,7 @@ import formatEmail from "@uth/utils/formatEmail"
 import { UserSchemaType } from "@uth/schemas/user.schema"
 import { Controller, useForm } from "react-hook-form"
 import InputNumber from "../InputNumber"
+import { useEffect } from "react"
 
 
 export default function ProfileComponent() {
@@ -22,9 +23,24 @@ export default function ProfileComponent() {
     queryKey: ['profile'],
     queryFn: userApi.getProfile
   })
+
+  const updateProfileMutation = useMutation({
+    mutationFn: userApi.updateProfile
+  })
   const profile = profileData?.result.user_profile
 
-  console.log(profile?.email)
+  useEffect(() => {
+    if(profile) {
+      console.log('run', profile.name)
+      setValue('name', profile.name)
+      setValue('avatar', profile.avatar)
+      setValue('phone', profile.phone)
+    }
+  }, [profile, setValue])
+
+  const onSubmit = handleSubmit(async (data) => {
+    // await updateProfileMutation.mutateAsync({})
+  })
 
   return (
     <div className="bg-white rounded-md px-2 md:px-7 pb-10 md:pb-20 shadow">
@@ -34,7 +50,7 @@ export default function ProfileComponent() {
           Manage and protect your account
         </div>
       </div>
-      <form className="mt-8 flex flex-col-reverse md:flex-row md:items-start">
+      <form onSubmit={onSubmit} className="mt-8 flex flex-col-reverse md:flex-row md:items-start">
         <div className="mt-6 flex-grow pr-12 md:mt-0">
           <div className="flex flex-wrap flex-col sm:flex-row">
             <div className="sm:w-[20%] truncate pt-3 sm:text-right capitalize">Username</div>
@@ -51,7 +67,7 @@ export default function ProfileComponent() {
           <div className="flex flex-wrap mt-6 flex-col sm:flex-row">
             <div className="sm:w-[20%] truncate pt-3 sm:text-right capitalize">Name</div>
             <div className="sm:w-[80%] sm:pl-5">
-              <Input className="mt-0" type='text' errorMessage={errors.name?.message} name='name' register={register} classNameInput="w-full rounded-md border border-gray-300 px-4 py-2 outline-none focus:border-gray-500 focus:shadow-sm" />
+              <Input placeholder="Name" className="mt-0" type='text' errorMessage={errors.name?.message} name='name' register={register} classNameInput="w-full rounded-md border border-gray-300 px-4 py-2 outline-none focus:border-gray-500 focus:shadow-sm" />
             </div>
           </div>
           <div className="flex flex-wrap mt-6 flex-col sm:flex-row">
@@ -61,15 +77,16 @@ export default function ProfileComponent() {
                 control={control}
                 name='phone'
                 render={({field}) => (
-                  <InputNumber className="mt-0" type="number" errorMessage={errors.phone?.message}  classNameInput="w-full rounded-md border border-gray-300 px-4 py-2 outline-none focus:border-gray-500 focus:shadow-sm"
+                  <InputNumber className="mt-0" placeholder="Number phone" errorMessage={errors.phone?.message}  classNameInput="w-full rounded-md border border-gray-300 px-4 py-2 outline-none focus:border-gray-500 focus:shadow-sm"
                     {...field}
+                    value={field.value || ''}
                     onChange={field.onChange}
                   />
                 )}
               />
             </div>
           </div>
-          <div className="mt-6 flex flex-wrap flex-col sm:flex-row">
+          {/* <div className="mt-6 flex flex-wrap flex-col sm:flex-row">
             <div className="sm:w-[20%] truncate pt-3 sm:text-right capitalize">BirthDay</div>
             <div className="sm:w-[80%] sm:pl-5">
               <div className="flex justify-between">
@@ -84,10 +101,10 @@ export default function ProfileComponent() {
                 </select>
               </div>
             </div>
-          </div>
+          </div> */}
           <div className="flex flex-wrap mt-6 flex-col sm:flex-row">
             <div className="sm:w-[20%] sm:mr-5 pt-3"></div>
-            <Button type="button" className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-8 py-2.5 text-center mt-4">Save</Button>
+            <Button type="submit" className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-8 py-2.5 text-center mt-4">Save</Button>
           </div>
         </div>
         <div className="flex justify-center md:w-72 md:border-l md:border-l-gray-200">
@@ -98,7 +115,7 @@ export default function ProfileComponent() {
                 className="w-full h-full rounded-full object-cover"
               />
             </div>
-            <Input className="mt-0" name='avatar' errorMessage={errors.avatar?.message} register={register} classNameInput="hidden" type="file"  />
+            <Input className="mt-0" name='avatar' errorMessage={errors.avatar?.message} register={register} classNameInput="hidden" type="file" />
             <button type="button" className="h-10 flex items-center justify-end rounded-md border bg-white px-6 text-sm text-gray-600 shadow-sm">
               Select Image
             </button>
