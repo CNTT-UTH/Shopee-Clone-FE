d2b6e2bc714300fe67d09fce81b6deb924ae0702
ductaip <dtai858@gmail.com>
2025-02-17 15:16:48 +0700

    done ui sort product list

--- src/pages/ProductList/components/Filter/Filter.tsx
+++ src/pages/ProductList/components/Filter/Filter.tsx
@@ -2,13 +2,15 @@ import path from '@uth/constants/path'
 import { AiOutlineMenuUnfold } from "react-icons/ai";
 import { Link } from 'react-router-dom'
 import { FaFilter } from "react-icons/fa";
+import Input from '@uth/components/Input';
+import Button from '@uth/components/Button';
 
 
 export default function Filter() {
   return <div className="py-4">
     <Link to={path.home} className='flex items-center font-bold gap-4'>
       <AiOutlineMenuUnfold size={18}/>
-      All Categories
+      Tất cả Danh mục
     </Link>
     <div className="bg-gray-300 h-[1px] my-4" /> 
     <ul className='text-sm'>
@@ -30,16 +32,31 @@ export default function Filter() {
     </ul>
     <Link to={path.home} className='mt-12 flex font-bold items-center gap-4'>
       <FaFilter />
-      Search Filter
+      Bộ lọc tìm kiếm
     </Link>
     <div className="bg-gray-300 h-[1px] my-4" /> 
     <div className="my-5">
       <div>Khoảng Giá</div>
       <form action="" className="mt-2">
-        <div className="flex items-center">
-          
+        <div className="flex items-start mt-4">
+          <Input 
+            type='text'
+            name='from'
+            className='grow'
+            placeholder='₫ TỪ'
+            classNameInput='text-sm outline-none border-2 rounded-md focus:border-gray-500 border-gray-300 w-full p-1 pl-3 focus:shadow-md' 
+          />
+          <div className="mx-2 mt-2 shrink-0">-</div>
+          <Input 
+            type='text'
+            name='to'
+            className='grow'
+            placeholder='₫ ĐẾN'
+            classNameInput='text-sm outline-none border-2 rounded-md focus:border-gray-500 border-gray-300 w-full p-1 pl-3 focus:shadow-md' 
+          />
         </div>
+        <Button className='w-full p-2 uppercase rounded-lg bg-orange text-white text-sm hover:opacity-70 flex justify-center items-center'>ÁP DỤNG</Button>
       </form>
-    </div>
+    </div> 
   </div>
 }
--- src/pages/ProductList/components/SortProductList/SortProductList.tsx
+++ src/pages/ProductList/components/SortProductList/SortProductList.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { GrFormPrevious, GrFormNext } from "react-icons/gr";
 
 export default function Sort() {
-  return (
-    <div>
-      sort product list
+  return <div className='bg-gray-300/40 py-4 px-3'>
+    <div className="flex flex-wrap items-center justify-between gap-2">
+      <div className="flex items-center flex-wrap gap-2">
+        <div>Sắp xếp theo</div>
+        <select name="" className='h-8 px-4 capitalize bg-white text-black text-sm hover:bg-slate-300 text-center'>
+          <option value="" disabled>Giá</option>
+          <option value="price:asc">Giá: Thấp đến Cao</option>
+          <option value="price:des">Giá: Cao đến Thấp</option>
+        </select>
+      </div>
+      <div className="flex items-center">
+        <div>
+          <span className="text-orange">1</span>
+          <span>/9</span>
+        </div>
+        <div className="ml-2 flex">
+          <button className="px-3 h-8 pr-1 rounded-tl-md rounded-bl-md bg-white/60 hover:bg-slate-100 cursor-not-allowed">
+            <GrFormPrevious />
+          </button>
+          <div className="w-[1px] bg-gray-200"></div>
+          <button className="px-3 h-8 pl-1 rounded-tr-md rounded-br-md bg-white/60 hover:bg-slate-100">
+            <GrFormNext />
+          </button>
+        </div>
+      </div>
     </div>
-  )
+  </div>
 }
