30360f918d51f60a1ce595fd1c5db96c18b1a251
ductaip <dtai858@gmail.com>
2025-03-02 03:37:26 +0700

    handle sort asc and des into productlist

--- src/pages/ProductList/ProductList.tsx
+++ src/pages/ProductList/ProductList.tsx
@@ -3,16 +3,30 @@ import Filter from "./components/Filter";
 import SortProductList from "./components/SortProductList";
 import useQueryParams from "@uth/hooks/useQueryParams";
 import { useProductAll } from "@uth/queries/useProduct";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Product as ProductType } from "@uth/types/product.type";
 
 export default function ProductList() {
   const [sortPrice, setSortPrice] = useState<string>("")
   const queryParam: any = useQueryParams()
   const {data} = useProductAll(queryParam)
-  
-  const sortedProductsAscending = data?.result.data.sort((a, b) => {
-    return (a.product_price?.price || 0) - (b.product_price?.price || 0);
-  }); 
+  const [sortedProducts, setSortedProducts] = useState<ProductType[] | undefined>(data?.result.data)  
+
+  useEffect(() => {
+    if (data?.result?.data && sortPrice) { 
+      const newSortedProducts = [...data.result.data];  
+
+      // console.log('run', sortPrice)
+      if (sortPrice === "price:asc") {
+        newSortedProducts?.sort((a, b) => (a.product_price?.price || 0) - (b.product_price?.price || 0)) 
+        setSortedProducts(newSortedProducts);
+      } else if (sortPrice === "price:des") {
+        newSortedProducts?.sort((a, b) => (b.product_price?.price || 0) - (a.product_price?.price || 0))
+        setSortedProducts(newSortedProducts);
+      }
+
+    }
+  }, [sortPrice, data]);
 
   return <div className="bg-gray-200 py-6">
     <div className="container">
@@ -23,7 +37,7 @@ export default function ProductList() {
          <div className="col-span-9">
            <SortProductList setSortPrice={setSortPrice} />
            <div className="mt-6 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3">
-            {data?.result.data.map((product) => (
+            {(sortedProducts || data?.result.data)?.map((product) => (
               <div className="col-span-1" key={product.product_id}>
                 <Product product={product} />
               </div>
