e422b59860a2d2d56991c00d039dc9308254f598
ductaip <dtai858@gmail.com>
2025-03-05 06:50:22 +0700

    REACT19: fix error that is breaking change about order DOM

--- src/pages/ProductDetail/ProductDetail.tsx
+++ src/pages/ProductDetail/ProductDetail.tsx
@@ -4,18 +4,25 @@ import { FaChevronLeft, FaChevronRight, FaMinus, FaPlus } from "react-icons/fa";
 import { useParams } from 'react-router-dom'
 import cart from '@uth/assets/images/cart.svg'
 import { Product } from '@uth/types/product.type';
-import { useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
+import des from '@uth/assets/des/des';
+import { sanitizeInput } from '@uth/utils/sanitize';
 export default function ProductDetail() {
   const {id} = useParams()
   const [currentIndexImages, setCurrentIndexImages] = useState([0, 5])
+  const [imgActive, setImgActive] = useState('')
 
-  const {data} = useProductDetail(id as string)
+  const {data, isLoading} = useProductDetail(id as string)
   const productData = data?.result
-  if(!productData) return
   const tmp = +Math.floor((Math.random() * 20000 + 1000) / 1000).toFixed(1) 
+  const currentImages = useMemo(() => (productData?.image_urls?.slice(...currentIndexImages) || []), [productData])
+
+  // useEffect(() => {
+  //   if(productData) setImgActive(productData.image_urls?.[0] as string)
+  // }, [productData])
   
   const next = () => {
-    if (currentIndexImages[1] < (productData?.image_urls!.length)) {
+    if (productData && (currentIndexImages[1] < (productData?.image_urls!.length))) {
       setCurrentIndexImages((prev) => [prev[0] + 1, prev[1] + 1])
     }
   }
@@ -24,13 +31,23 @@ export default function ProductDetail() {
     if (currentIndexImages[0] > 0) {
       setCurrentIndexImages((prev) => [prev[0] - 1, prev[1] - 1])
     }
+  } 
+  if(isLoading) { 
+    <div role="status">
+      <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+      </svg>
+      <span className="sr-only">Loading...</span>
+    </div> 
   }
 
   return (
+    productData &&
     <div className='bg-gray-200 py-6'>
       <div className="container"> 
-        <div className="bg-white p-4 shadow rounded-md">
-          <div className="grid grid-cols-12 gap-9">
+        <div className="bg-white p-10 shadow rounded-md">
+          <div className="grid grid-cols-12 gap-12">
             <div className="col-span-5">
               <div className="relative w-full pt-[100%] shadow">
                 <img 
@@ -43,7 +60,7 @@ export default function ProductDetail() {
                 <button className='absolute left-0 top-1/2 z-10 h-9 w-5 -translate-y-1/2 bg-black/20 text-white'>
                   <FaChevronLeft />
                 </button>
-                {productData.image_urls?.slice(0, 5).map((image, index) => {
+                {currentImages?.map((image, index) => {
                   const isActive = index === 0
                   return (
                     <div key={index} className='relative w-full pt-[100%]'>
@@ -87,16 +104,23 @@ export default function ProductDetail() {
                   </div>
                 </div>
 
-                <div className='flex mt-8'>
-                  <div className='mr-2'>{productData?.options?.[0].name}:</div>
-                  <div className='flex items-center ml-4 -mt-1 flex-wrap gap-2'>
-                    {productData.options?.[0].value?.map((item, index) => {
-                      return <div className='border px-4 py-1 rounded-md shadow cursor-pointer hover:shadow-lg'>{item}</div>
-                    })}
-                  </div>
+                <div className='mt-16'>
+                  {productData.options?.map((value, index) => {
+                    return (
+                      <div key={index} className='grid grid-cols-12 mt-8'>
+                        <div className='col-span-2'>{value.name}:</div>
+                        <div key={index} className='flex items-center col-span-10 -mt-1 flex-wrap gap-2'>
+                          {value.value?.map((item, index) => {
+                            return <div key={index} className='border px-4 py-1 rounded-md shadow cursor-pointer hover:shadow-lg'>{item}</div>
+                          })}
+                        </div>
+                      </div>
+                    )
+                  })}
+                  
                 </div>
 
-                <div className="mt-8 mr-2 flex items-center">
+                <div className="mt-12 mr-2 flex items-center">
                   <div className="capitalize">Số lượng:</div>
                   <div className="ml-4 flex items-center">
                     <button className="flex h-8 w-8 items-center justify-center rounded-l-sm border border-gray-300 text-gray-600">
@@ -111,7 +135,7 @@ export default function ProductDetail() {
                   <div className="ml-6 text-sm text-gray-500">{tmp * 2379} sản phẩm có sẵn</div>
                 </div>
 
-                <div className="mt-8 flex items-center">
+                <div className="mt-12 flex items-center">
                   <button className="flex h-12 items-center justify-center rounded-sm border border-orange bg-orange/10 px-5 capitalize text-orange shadow-sm hover:bg-orange/5">
                     <svg
                       enableBackground='new 0 0 15 15'
@@ -147,9 +171,14 @@ export default function ProductDetail() {
       <div className="container">
         <div className="mt-8 bg-white p-4 shadow">
           <div className="rounded bg-gray-50 p-4 text-lg capitalize text-slate-700">Mô tả sản phẩm</div>
-          <div className="mx-4 mt-12 mb-4 text-sm leading-loose"></div>
+          <div className="mx-4 mt-12 mb-4 text-sm leading-loose">
+            <div dangerouslySetInnerHTML={{
+              __html: sanitizeInput(des[0])
+            }}/>
+            <img src={productData?.image_urls?.[0]} className='w-[60%] mt-4' alt="" />
+          </div>
         </div>
       </div>
-    </div>
+    </div> 
   )
 }
